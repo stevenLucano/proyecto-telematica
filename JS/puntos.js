5a812a1876252a02e8b46c01117399131db4e97e
stevenLucano <st.lucano1205@gmail.com>
2022-02-26 23:37:58 -0500

    puntos decorado

---
+++ JS/puntos.js
@@ -0,0 +1,69 @@
+const contenido = document.getElementById("contenido");
+const tablaCont = document.getElementById("tabla-registros").content;
+const fragment = document.createDocumentFragment();
+const mapaPrueba = document.getElementById("map1");
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems,{
+        coverTrigger: false,
+        constrainWidth: false
+    });
+
+    var elems2 = document.querySelectorAll('.modal');
+    var instances2 = M.Modal.init(elems2, {});
+});
+
+function iniciarMap(){
+    let coord = {lat: 3.4743554,lng:-76.5118369};
+    let map = new google.maps.Map(document.getElementById("map1"),{
+        zoom:15,
+        center: coord
+    });
+    let marker = new google.maps.Marker({
+        position: coord,
+        map: map,
+        title: `<h2>Hola mundo</h2>`
+    })
+}
+
+const btnCambio = document.getElementById("btn-cambio");
+
+btnCambio.addEventListener('click', e => {
+    alternar(e);
+})
+
+const alternar = e => {
+    if(e.target.classList.contains("r-tabla")){
+        btnCambio.innerHTML = "ver mapa";
+        btnCambio.classList.remove("r-tabla");
+        btnCambio.classList.add("r-mapa");
+
+        mapaPrueba.style.display = "none";
+        const cloneTabla = tablaCont.cloneNode(true);
+        fragment.appendChild(cloneTabla);
+        contenido.appendChild(fragment);
+    } else if(e.target.classList.contains("r-mapa")){
+        btnCambio.innerHTML = "ver tabla";
+        btnCambio.classList.remove("r-mapa");
+        btnCambio.classList.add("r-tabla");
+
+        const registros = document.getElementById("registros");
+        contenido.removeChild(registros);
+        mapaPrueba.style.display = "";
+    }
+}
+
+const registrosT = document.getElementById("registros");
+registrosT.addEventListener('click', e => {
+    cambiarBoton(e);
+})
+
+
+const cambiarBoton = e => {
+    if(e.target.id[0]==="e"){
+        const id = document.getElementById(`r-${e.target.id[e.target.id.length-1]}`);
+        const modal = document.getElementById("modal-e");
+        modal.innerHTML = `Desea eliminar el registro ${id.innerHTML}`;
+    }
+}
--- JS/solicitud_punto.js
+++ JS/solicitud_punto.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
         coverTrigger: false,
         constrainWidth: false
     });
-    var elems2 = document.querySelectorAll('.carousel');
-    var instances2 = M.Carousel.init(elems2, {
+    let elems2 = document.querySelectorAll('.carousel');
+    let instances2 = M.Carousel.init(elems2, {
         indicators: true,
         duration: 100
     });
@@ -14,27 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances3 = M.Modal.init(elems3, {});
 });
 
-// console.log("Hello")
-// function iniciarMapa(){
-//     let coord = {lat: 3.4743554,lng:-76.5118369};
-//     let coord2 = {lat: 3.471992, lng:-76.5117772};
+function iniciarMap(){
+    let coord = {lat: 3.4743554,lng:-76.5118369};
+    // let coord2 = {lat: 3.471992, lng:-76.5117772};
 
-//     const image =
-//     "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+    // const image =
+    // "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
 
-//     let map = new google.maps.Map(document.getElementById("map1"),{
-//         zoom:15,
-//         center: coord
-//     });
-//     let marker = new google.maps.Marker({
-//         position: coord,
-//         map: map,
-//         title: `<h2>Hola mundo</h2>`
-//     })
-//     let marker2 = new google.maps.Marker({
-//         position: coord2,
-//         map: map,
-//         title: "Hello world",
-//         icon: image
-//     })
-// }
+    let map = new google.maps.Map(document.getElementById("map"),{
+        zoom:15,
+        center: coord
+    });
+    let marker = new google.maps.Marker({
+        position: coord,
+        map: map,
+        title: `<h2>Hola mundo</h2>`
+    })
+    // let marker2 = new google.maps.Marker({
+    //     position: coord2,
+    //     map: map,
+    //     title: "Hello world",
+    //     icon: image
+    // })
+}
